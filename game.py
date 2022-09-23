8d14547de0061e2e2dbc3616b4a648ffb41edea0
Navarro <navarro.nextage@gmail.com>
2022-09-23 00:36:51 -0300

    feat: FPS counter and limiter with console frame display

--- game.py
+++ game.py
@@ -8,6 +8,9 @@ name: str
 window: pygame.display
 __is_initialized: bool = False
 __is_running: bool = False
+__clock = pygame.time.Clock()
+__elapsed: float
+framerate: int = 0
 
 def init(game_name: str, window_size: Tuple[int, int]) -> None:
 	global name, window, __is_initialized
@@ -40,10 +43,27 @@ def __handle_event() -> None:
 		elif event.type == pygame.KEYDOWN:
 			render_obj(Object((10.0, 12.0), (25.0, 12.0), (255, 0, 255)))
 		
+def __framerate_counter() -> None:
+	global __clock, __elapsed, framerate
+
+	__elapsed = __clock.tick_busy_loop(framerate)
+	fps = int(__clock.get_fps())
+	print('FPS: ', fps)
+
+@property
+def elapsed() -> int:
+	global __elapsed
+
+	return __elapsed
+
+
 def __loop() -> None:
 	global __is_running
 
 	while (__is_running):
+		# handle time since the last tick and count fps
+		__framerate_counter()
+
 		# handle events: keyboard, mouse, etc...
 		__handle_event()
 
