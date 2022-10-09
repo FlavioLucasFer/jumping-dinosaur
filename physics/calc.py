313c6ba40d4441d19b4d0b545b2cae8ab9282cd4
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-10-09 00:37:54 -0300

    doc: uniformly_variable_motion

--- physics/calc.py
+++ physics/calc.py
@@ -1,9 +1,27 @@
 from physics.gravity import SolarSystemGravityAccelerations
 
+def uniformly_variable_motion(
+    S0: float,
+    V0: float,
+    a: float,
+    t: float
+) -> float:
+    """
+    Calculate uniformly variable motion
+    
+    S = S0 + V0.t + (a.t²) / 2
 
-def uniformly_variable_motion(So, Vo, a, t) -> float:
-    # S = So + Vo.t + (a.t²) / 2
-    return So + Vo * t + (a * t ** 2) / 2.0
+    Args:
+        S0 (float): initial position
+        V0 (float): initial velocity
+        a (float): acceleration
+        t (float): time
+
+    Returns
+        float: uniformly variable motion result
+    """
+    # S = S0 + V0.t + (a.t²) / 2
+    return S0 + V0 * t + (a * t ** 2) / 2.0
 
 
 def weight_calc(a: float, m: float) -> float:
