77eb7d3bad86697d68fdd9df1d31002eaaa2bcb1
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-10-23 03:03:04 -0300

    refactor: game module identation

--- engine/game.py
+++ engine/game.py
@@ -8,78 +8,79 @@ from pygame.font import init as init_font
 from pygame.time import Clock as PyClock
 
 
-__game: Game = None
+_game: Game = None
 
 
 class WindowRecomendedResolutions(Enum):
-    P240 = (426, 240)
-    P360 = (640, 360)
-    P480 = (854, 480)
-    HD = (1280, 720)
-    FULL_HD = (1920, 1080)
-    QUAD_HD = (2560, 1440)
-    FOUR_K = (3840, 2160)
-    EIGHT_K = (7680, 4320)
-
-
-class Clock:
-    __elapsed: float
-    __clock = PyClock()
-
-    def tick_busy_loop(self, framerate=0) -> int:
-        tick = self.__clock.tick_busy_loop(framerate)
-        self.__elapsed = tick / 1000.0
-        return tick
-
-    def tick(self, framerate=0) -> int:
-        tick = self.__clock.tick(framerate)
-        self.__elapsed = tick / 1000.0
-        return tick
-
-    @property
-    def elased(self) -> float:
-        return self.__elapsed
-
-    @property
-    def fps(self) -> float:
-        return self.__clock.get_fps()
+	P240 = (426, 240)
+	P360 = (640, 360)
+	P480 = (854, 480)
+	HD = (1280, 720)
+	FULL_HD = (1920, 1080)
+	QUAD_HD = (2560, 1440)
+	FOUR_K = (3840, 2160)
+	EIGHT_K = (7680, 4320)
 
 
 class Game:
-    __is_initialized = False
-    __window: Surface
-    name: str
+	__is_initialized = False
+	__window: Surface
+	name: str
 
-    def __new__(cls: Game) -> Game:
-        global __game
-        if not __game:
-            __game = object.__new__(cls)
-        return __game
+	def __new__(cls: Game, name: str) -> Game:
+		global _game
+		if _game == None:
+			_game = object.__new__(cls)
+		return _game
 
-    def __init__(self, name: str) -> None:
-        self.name = name
-        
-    def init(self, 
-            window_size: WindowRecomendedResolutions.FULL_HD.value) -> None:
-        if self.__is_initialized:
-            raise Exception('Game is already initialized')
+	def __init__(self, name: str) -> None:
+		self.name = name
+		
+	def init(self, 
+			window_size=WindowRecomendedResolutions.HD.value) -> None:
+		if self.__is_initialized:
+			raise Exception('Game is already initialized')
 
-        init_pygame()
-        init_display()
-        init_font()
+		init_pygame()
+		init_display()
+		init_font()
 
-        self.window = window_size
-        self.__is_initialized = True
+		self.window = window_size
+		self.__is_initialized = True
 
-    def quit() -> None:
-        global __game
-        pygame_quit()
-        __game = None
+	def quit(self) -> None:
+		global __game
+		pygame_quit()
+		__game = None
+		del self
 
-    @property
-    def window(self) -> Surface:
-        return self.__window
+	@property
+	def window(self) -> Surface:
+		return self.__window
 
-    @window.setter
-    def window(self, size: Tuple[int, int]) -> None:
-        self.__window = display_set_mode(size)
+	@window.setter
+	def window(self, size: Tuple[int, int]) -> None:
+		self.__window = display_set_mode(size)
+
+
+class Clock:
+	__elapsed: float
+	__clock = PyClock()
+
+	def tick_busy_loop(self, framerate=0) -> int:
+		tick = self.__clock.tick_busy_loop(framerate)
+		self.__elapsed = tick / 1000.0
+		return tick
+
+	def tick(self, framerate=0) -> int:
+		tick = self.__clock.tick(framerate)
+		self.__elapsed = tick / 1000.0
+		return tick
+
+	@property
+	def elased(self) -> float:
+		return self.__elapsed
+
+	@property
+	def fps(self) -> float:
+		return self.__clock.get_fps()
--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-import game
-from object import Object
-from vector import Vector
-from physics.gravity import SolarSystemGravityAccelerations, g_to_mps2
+from unicodedata import name
+from engine.game import Game
 
+game = Game('jumping dinosaur')
+game.init()
 
-obj = Object(Vector(10.0, 12.0, 0.0), Vector(12.0, 25.0, 0.0), (255, 0, 255))
-obj.vel = Vector(0.0, 0.0, 0.0)
-obj.mass = 0.1
-obj.enable_free_fall(g_to_mps2(SolarSystemGravityAccelerations.SUN.value))
-game.render_obj(obj)
+while True:
+    print('running')
 
-
-game.init(
-    game_name='jumping-dinosaur',
-    window_size=(800, 600),
-)
-game.run()
+game.quit()
